e58b9b2eb2e74b9c2172a48876215763041cc780
anajullia <ana.carvalho331@etec.sp.gov.br>
2024-10-14 01:25:26 -0300

    Front End

--- php/index.php
+++ php/index.php
@@ -10,43 +10,72 @@ $produtos_selecionados = isset($_COOKIE['produtos_selecionados']) ? explode(',',
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loja de Produtos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body class="container mt-5">
 
-    <h1 class="mb-4">Selecione seus produtos</h1>
+    <h1 class="mb-4 titulo">Selecione seus produtos</h1>
     <form method="POST" action="processar.php">
-        <div class="containercompra d-flex flex-row ">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="produtos[]" value="Produto A" 
-                    <?php echo in_array('Produto A', $produtos_selecionados) ? 'checked' : ''; ?>>
-                <label class="form-check-label">Produto A</label>
+        <div class="divbotoes d-flex flex-row gap-3">
+            <button type="submit" class="btn btn-primary mt-3 btad">Adicionar ao Carrinho</button>
+            <a href="carrinho.php" class="btn btn-secondary mt-3 btad">Ver Carrinho</a>
+        </div>
+            <div class="containercompra d-flex flex-row gap-3">
+        
+            <div class="form-check d-flex flex-column divprod">
+                <div class="imgprod"> 
+                    <img src="../assets/camisa1930.webp" width=200></img>
+                </div>
+                <input class="form-check-input checkprod" type="checkbox" name="produtos[]" value="Camisa SP - R. 1930" 
+                    <?php echo in_array('Camisa SP - R. 1930', $produtos_selecionados) ? 'checked' : ''; ?>>
+                <label class="form-check-label labelprod">Camisa Goleiro São Paulo Retrô - 1988</label>
+                <label class="form-check-label labelprod labelpreco">R$299,90</label>
             </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="produtos[]" value="Produto B" 
-                    <?php echo in_array('Produto B', $produtos_selecionados) ? 'checked' : ''; ?>>
-                <label class="form-check-label">Produto B</label>
+
+            <div class="form-check d-flex flex-column divprod">
+                <div class="imgprod"> 
+                    <img src="../assets/camisa1966.jpg" width=200></img>
+                </div>
+                <input class="form-check-input checkprod" type="checkbox" name="produtos[]" value="Camisa SP - R. 1966" 
+                    <?php echo in_array('Camisa SP - R. 1966', $produtos_selecionados) ? 'checked' : ''; ?>>
+                <label class="form-check-label labelprod">Camisa Goleiro São Paulo Retrô - 1988</label>
+                <label class="form-check-label labelprod labelpreco">R$279,90</label>
             </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="produtos[]" value="Produto C" 
-                    <?php echo in_array('Produto C', $produtos_selecionados) ? 'checked' : ''; ?>>
-                <label class="form-check-label">Produto C</label>
+
+            <div class="form-check d-flex flex-column divprod">
+                <div class="imgprod"> 
+                    <img src="../assets/camisagoleiro1988.webp" width=200></img>
+                </div>
+                <input class="form-check-input checkprod" type="checkbox" name="produtos[]" value="Camisa SP Gol. - R. 1988" 
+                    <?php echo in_array('Camisa SP Gol. - R. 1988', $produtos_selecionados) ? 'checked' : ''; ?>>
+                <label class="form-check-label labelprod">Camisa Goleiro São Paulo Retrô - 1988</label>
+                <label class="form-check-label labelprod labelpreco">R$1.222,90</label>
             </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="produtos[]" value="Produto D" 
-                    <?php echo in_array('Produto D', $produtos_selecionados) ? 'checked' : ''; ?>>
-                <label class="form-check-label">Produto D</label>
+
+            <div class="form-check d-flex flex-column divprod">
+                <div class="imgprod"> 
+                    <img src="../assets/camisagoleiro1992.webp" width=200></img>
+                </div>
+                <input class="form-check-input checkprod" type="checkbox" name="produtos[]" value="Camisa SP Gol. - R. 1992" 
+                    <?php echo in_array('Camisa SP Gol. - R. 1992', $produtos_selecionados) ? 'checked' : ''; ?>>
+                <label class="form-check-label labelprod">Camisa Goleiro São Paulo Retrô - 1988</label>
+                <label class="form-check-label labelprod labelpreco">R$479,90</label>
             </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="produtos[]" value="Produto E" 
-                    <?php echo in_array('Produto E', $produtos_selecionados) ? 'checked' : ''; ?>>
-                <label class="form-check-label">Produto E</label>
+
+            <div class="form-check d-flex flex-column divprod">
+                <div class="imgprod"> 
+                    <img src="../assets/camisauruguai.webp" width=200></img>
+                </div>
+                <input class="form-check-input checkprod" type="checkbox" name="produtos[]" value="Camisa SP - Uruguai" 
+                    <?php echo in_array('Camisa SP - Uruguai', $produtos_selecionados) ? 'checked' : ''; ?>>
+                <label class="form-check-label labelprod">Camisa São Paulo - Uruguai</label>
+                <label class="form-check-label labelprod labelpreco">R$299,90</label>
             </div>
+            
         </div>
 
-        <button type="submit" class="btn btn-primary mt-3">Adicionar ao Carrinho</button>
+        
     </form>
-
-    <a href="carrinho.php" class="btn btn-secondary mt-3">Ver Carrinho</a>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
